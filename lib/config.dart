1135358ec74e9dbb37e33d19a74bfee85cea74f0
hritik <hritikc3961@gmail.com>
2022-09-15 12:29:16 -0700

    update config file path

--- lib/config.dart
+++ lib/config.dart
@@ -20,7 +20,8 @@ class _GetConfigState extends ConsumerState<GetConfig> {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       final configStateProvider = ref.read(ConfigStateprovider.notifier);
 
-      String configFilePath = '/Users/Smart/Desktop/test/dashboard_config.yaml';
+      String configFilePath = '/etc/xdg/AGL/dashboard_config.yaml';
+
 
 
       final configFile = File(configFilePath);
